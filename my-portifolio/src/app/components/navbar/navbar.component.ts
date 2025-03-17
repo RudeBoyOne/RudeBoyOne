fd3c8e6f5f63917966349ec0853d82ea207997e1
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-03-17 19:55:46 -0300

    REFACTOR: atualização da navbar e conteúdo home

--- my-portifolio/src/app/components/navbar/navbar.component.ts
+++ my-portifolio/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+    collapseNavbar(): void {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+
+        if (navbarCollapse?.classList.contains('show')) {
+            const onTransitionEnd = () => {
+                navbarCollapse?.classList.remove('show');
+                navbarCollapse?.removeEventListener('transitionend', onTransitionEnd);
+            };
+
+            navbarCollapse.addEventListener('transitionend', onTransitionEnd);
+            navbarCollapse.classList.remove('collapsing');
+        }
+    }
 
 }
